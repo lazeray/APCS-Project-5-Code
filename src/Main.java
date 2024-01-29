5353ccd02e379c8e51ad4e3b72a0ac593db05a32
Alex Ge <alexalexgege@gmail.com>
2024-01-28 23:53:18 -0800

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
-    static final int BOARDX = 7;
-    static final int BOARDY = 15;
+    static final int BOARDX = 5;
+    static final int BOARDY = 5;
     public static void main(String[] args) {
         System.out.println("Welcome to MEGA Connect 5!");
         System.out.println("You may choose to place one 3x3 MegaPiece");
@@ -16,8 +16,35 @@ public class Main {
         while (true) {
             board.displayAll();
             System.out.println("Player: " + player + ":");
+            if (remainingMegaPiece[player-1] == 1) {
+                System.out.println("Use MegaPiece? (Y/N)?");
+                char inp = input.next().charAt(0);
+                if (inp == 'Y' || inp == 'y') {
+                    System.out.println("Enter the column you would like to place your MegaPiece at. ");
+                    int in = input.nextInt();
+                    inp--;
+                    MegaPiece p = new MegaPiece(in, player, board);
+                    try {
+                        board.placePiece(p);
+                        remainingMegaPiece[player-1]--;
+                        player = player % 2 + 1;
+                    } catch (IllegalArgumentException ex) {
+                        System.out.println("Not a valid placement.");
+                    }
+                    continue;
+                }
+            }
 
-            // Stuff should go here, probably
+            System.out.println("Enter the column you would like to place your Piece at. ");
+            int in = input.nextInt();
+            in--;
+            Piece p = new Piece(in, player, board);
+            try {
+                board.placePiece(p);
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Not a valid placement.");
+                continue;
+            }
 
             if (board.checkForWin(player)) {
                 System.out.println("player: " + player + " has won!");
@@ -26,8 +53,5 @@ public class Main {
             }
             player = player % 2 + 1;
         }
-
-
-
     }
 }
